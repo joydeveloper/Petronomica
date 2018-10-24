0ae661aa38462b7ba6d22971e955e60cc0be1415
joydeveloper <joy2431@list.ru>
2018-10-24 07:16:21 +0700

    Services html,misc html

--- Petronomica/Petronomica/Petronomica/App_Code/ListHelper.cs
+++ Petronomica/Petronomica/Petronomica/App_Code/ListHelper.cs
@@ -15,6 +15,15 @@ namespace HtmlHelpersApp.App_Code
             result += "</ul>";
             return new HtmlString(result);
         }
-      
+        public static HtmlString CreateA(this IHtmlHelper html, string[] items)
+        {
+            string result = "<ol>";
+            foreach (string item in items)
+            {
+                result += $"<li><a href={item} target='_blank'>{item}</a></li>";
+            }
+            result += "</ol>";
+            return new HtmlString(result);
+        }
     }
 }

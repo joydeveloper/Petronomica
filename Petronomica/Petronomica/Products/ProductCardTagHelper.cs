0657501ac7ce43ebb07e7d2fcf0df9c2c50e3dcb
joydeveloper <joy2431@list.ru>
2018-10-28 05:14:27 +0700

    design,modal without post form

--- Petronomica/Petronomica/Products/ProductCardTagHelper.cs
+++ Petronomica/Petronomica/Products/ProductCardTagHelper.cs
@@ -7,15 +7,19 @@ namespace Products
 {
     public class ProductCardTagHelper : TagHelper
     {
+        Random rand = new Random();
+        int i;
+        string[] colors= new string[] { "alert-success", "alert-warning", "alert-primary", "alert-danger", "alert-light" };
         public Product Product { get; set; }
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
+            i = rand.Next(0, 4);
             var template =
 $@"<div class='card-body'>
     <p class='card-text'>{Product.Name}</p>
       <div class='d-flex justify-content-between align-items-center'>
              <img id='{Product.Id}'  class='card-img-right' onclick='PreOrder({Product.Id})'  style='cursor:pointer' src={Product.Image}/>
-                 <h5 class='text-muted alert alert-info'>От {Product.Price} рублей</h5>
+                 <h7 class='text-muted alert {colors[i]}'>От {Product.Price} рублей</h7>
       </div>";
             output.TagName = "div";
             output.Attributes.SetAttribute("class", "card mb-6 shadow-sm");

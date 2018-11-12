ee63f4c7ba88189e8e012d87805c068b9a5a845d
joydeveloper <joy2431@list.ru>
2018-11-12 19:25:18 +0700

    portfolio setup

---
+++ Petronomica/Petronomica/Portfolio/PortfolioContainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Portfolio
+{
+    public class PortfolioItem
+    {
+        public int IdEmployee { get; set; }
+        public string Name { get; set; }
+        public float Rating { get; set; }
+        public string Path { get; set; }
+    }
+   
+}
---
+++ Petronomica/Petronomica/Portfolio/PotfolioTagHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Portfolio
+{
+
+    public class PotfolioTagHelper : TagHelper
+    {
+        Random rand = new Random();
+        int i;
+        string[] colors = new string[] { "alert-success", "alert-warning", "alert-primary", "alert-danger", "alert-light" };
+        PortfolioItem PortfolioItem { get; set; }
+                public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            i = rand.Next(0, 4);
+            var template =
+$@"<div class='card-body'>
+            <p class='card-text'>{PortfolioItem.Name}</p>
+              <div class='d-flex justify-content-between align-items-center'>
+                     <img id='{PortfolioItem.IdEmployee}'  class='card-img-right' onclick='PoductConfig({PortfolioItem.Path})'  style='cursor:pointer' src={PortfolioItem.Rating}/>
+              </div>";
+            output.TagName = "div";
+            output.Attributes.SetAttribute("class", "card mb-1 shadow-sm");
+            output.Content.AppendHtml(template);
+            output.Content.AppendHtml("</div>");
+        }
+    }
+   
+}
